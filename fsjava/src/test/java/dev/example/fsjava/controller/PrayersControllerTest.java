05608bb71fe0c9b1753132e2a5e09e38a0eaf69e
Ahmet Ö <oezahm60@gmail.com>
2023-02-06 14:15:31 +0100

    Fixed PrayersControll UnitTest

--- fsjava/src/test/java/dev/example/fsjava/controller/PrayersControllerTest.java
+++ fsjava/src/test/java/dev/example/fsjava/controller/PrayersControllerTest.java
@@ -1,5 +1,6 @@
 package dev.example.fsjava.controller;
 
+import dev.example.fsjava.DAL.HTMLType;
 import dev.example.fsjava.service.ScraperServicePrayers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +18,9 @@ class PrayersControllerTest {
     private MockMvc mvc;
 
     @Test
-    void getTimeIntegrationTest() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.get("/time");
-        MvcResult result = mvc.perform(request).andReturn();
-        assertEquals("Hello",result.getResponse().getContentAsString());
-    }
-
-    @Test
     void getTimeUnitTest() throws Exception {
         PrayersController controller = new PrayersController();
-        //ScraperServicePrayers prayers = new ScraperServicePrayers();
-
-        //tring response = controller.getTime(prayers);
-        //assertTrue(response.contains("17:18"));
+        String response = controller.getTime();
+        assertTrue(response.contains("17:33"));
     }
 }
