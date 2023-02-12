b82ab85dd9c6bd3be89d8d4472559ec2f8d8f426
Ahmet Ö <oezahm60@gmail.com>
2023-02-12 16:18:34 +0100

    added CSS hoover effect and changed font style

--- fsjava/src/modal-project/src/App.vue
+++ fsjava/src/modal-project/src/App.vue
@@ -105,6 +105,12 @@ export default {
 </script>
 
 <style>
+@import 'https://fonts.googleapis.com/css?family=Playfair+Display:400,700i,900i';
+
+*{
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -117,8 +123,16 @@ export default {
   grid-template-columns: repeat(7, 1fr);
   grid-template-rows: repeat(3, 1fr);
   grid-column-gap: 10px;
-  grid-row-gap: 100px
+  grid-row-gap: 100px;
+  
+  font-family: 'Playfair Display', serif;
+  font-size: 22px;
 }
+
+.wrap{
+  perspective-origin: 50% 50%;
+}
+
 
 .showCity, .showTime{
   position: absolute;
@@ -128,9 +142,8 @@ export default {
 
 .showTime{
   margin-top: 35px;
+  border: 2px solid #636866;
 }
-
-.showTime 
 
 a,p{
   font-size: 20px;
@@ -142,13 +155,17 @@ a,p{
 }
 
 .headerLeftSection { grid-area: 1 / 1 / 2 / 4;}
-.headerRightSection { grid-area: 1 / 4 / 2 / 8; border: 1px solid #636866;}
-.content1 { grid-area: 2 / 1 / 4 / 2;}
-.content2 { grid-area: 2 / 2 / 4 / 3;}
-.content3 { grid-area: 2 / 3 / 4 / 4;}
-.content4 { grid-area: 2 / 5 / 4 / 6;}
-.content5 { grid-area: 2 / 6 / 4 / 7;}
-.content6 { grid-area: 2 / 7 / 4 / 8;}
+.headerRightSection { grid-area: 1 / 4 / 2 / 8; border: 1px solid #636866;transition: background-color 0.5s ease;}
+.content1 { grid-area: 2 / 1 / 4 / 2;transition: background-color 0.5s ease;}
+.content2 { grid-area: 2 / 2 / 4 / 3;transition: background-color 0.5s ease;}
+.content3 { grid-area: 2 / 3 / 4 / 4;transition: background-color 0.5s ease;}
+.content4 { grid-area: 2 / 5 / 4 / 6;transition: background-color 0.5s ease;}
+.content5 { grid-area: 2 / 6 / 4 / 7;transition: background-color 0.5s ease;}
+.content6 { grid-area: 2 / 7 / 4 / 8;transition: background-color 0.5s ease;}
+
+.headerRightSection:hover, .content1:hover, .content2:hover, .content3:hover, .content4:hover, .content5:hover, .content6:hover{
+  background-color: wheat;cursor: pointer;
+}
 
 .content1,.content2,.content3,.content4,.content5,.content6{
   box-shadow:
@@ -163,5 +180,4 @@ a,p{
   56.3px 63.6px 11.8px rgba(0, 0, 0, 0.168),
   100px 113px 21px rgba(0, 0, 0, 0.14);
 }
-
 </style>
