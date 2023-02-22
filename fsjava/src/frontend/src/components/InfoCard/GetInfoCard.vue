ac05cc229a140b05d609f5e882a588d8b9a268e2
Ahmet Ö <oezahm60@gmail.com>
2023-02-22 21:30:06 +0100

    added first logic to post new infobox check

--- fsjava/src/frontend/src/components/InfoCard/GetInfoCard.vue
+++ fsjava/src/frontend/src/components/InfoCard/GetInfoCard.vue
@@ -1,10 +1,17 @@
 <template>
   <ul v-for="task in json">
     {{ task.value }}| Check: {{ task.check }}
+    <select size="2" v-bind:id="task.ID">
+      <option>true</option>
+      <option>false</option>
+    </select>
   </ul>
+
+  <button @click="getJSONTEST()">SAVE</button>
 </template>
 
 <script>
+
 export default {
   name: 'GetInfoCard',
   props: {
@@ -25,8 +32,23 @@ export default {
         .then(data => {       
           const jsonData = JSON.parse(data);
           this.json = jsonData;
-          console.log(jsonData);
         }); 
+    },
+    getJSONTEST(){
+      let counter = 0;
+      for (let i = 0; i <= this.json.length - 1; i++) {
+        const select = document.getElementById(this.json[i].ID)
+
+        if(select.value != this.json[i].check.toString()){
+          this.postData(this.json[i].ID, select.value)
+        }
+        else{counter++}
+      }
+
+      if(counter == this.json.length){window.alert("no data to update!")}
+    },
+    async postData(ID, check){
+      console.log("UNGLEICH")
     }
   }
 }
